6f8bbdf4f9833e1d0255764649cdd3d0642529e8
Miguel Palau <miguelpalau1@gmail.com>
2025-01-25 13:30:40 -0500

    move haversine to util class

--- src/main/java/com/hackathon/inditex/service/impl/OrderAssignmentServiceImpl.java
+++ src/main/java/com/hackathon/inditex/service/impl/OrderAssignmentServiceImpl.java
@@ -8,6 +8,7 @@ import com.hackathon.inditex.mapper.OrderMapper;
 import com.hackathon.inditex.repository.CenterRepository;
 import com.hackathon.inditex.repository.OrderRepository;
 import com.hackathon.inditex.service.OrderAssignmentService;
+import com.hackathon.inditex.util.GeoUtil;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,7 +18,6 @@ import java.util.*;
 @Service
 public class OrderAssignmentServiceImpl implements OrderAssignmentService {
 
-    private static final int EARTH_RADIUS = 6371;
     private final OrderRepository orderRepository;
     private final CenterRepository centerRepository;
     private final OrderMapper orderMapper;
@@ -27,20 +27,6 @@ public class OrderAssignmentServiceImpl implements OrderAssignmentService {
         this.orderRepository = orderRepository;
         this.centerRepository = centerRepository;
         this.orderMapper = orderMapper;
-    }
-
-    private double haversine(double lat1, double lon1, double lat2, double lon2) {
-
-        double latDistance = Math.toRadians(lat2 - lat1);
-        double lonDistance = Math.toRadians(lon2 - lon1);
-
-        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
-                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
-                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
-
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-        return EARTH_RADIUS * c; // in km
     }
 
     @Override
@@ -78,7 +64,7 @@ public class OrderAssignmentServiceImpl implements OrderAssignmentService {
             double minDistance = Double.MAX_VALUE;
 
             for (Center center : availableCenters) {
-                double distance = haversine(
+                double distance = GeoUtil.haversine(
                         order.getCoordinates().getLatitude(),
                         order.getCoordinates().getLongitude(),
                         center.getCoordinates().getLatitude(),
---
+++ src/main/java/com/hackathon/inditex/util/GeoUtil.java
@@ -0,0 +1,21 @@
+package com.hackathon.inditex.util;
+
+public class GeoUtil {
+
+    private static final int EARTH_RADIUS = 6371;
+
+    public static double haversine(double lat1, double lon1, double lat2, double lon2) {
+
+        double latDistance = Math.toRadians(lat2 - lat1);
+        double lonDistance = Math.toRadians(lon2 - lon1);
+
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        // in km
+        return EARTH_RADIUS * c;
+    }
+}
